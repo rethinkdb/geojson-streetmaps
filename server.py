bad4c3035b3b7174fc767cefb9f93be3ef6ed47a
Josh Kuhn <josh@rethinkdb.com>
2014-09-10 14:05:37 -0700

    Initial commit

---
+++ server.py
@@ -0,0 +1,85 @@
+import argparse
+import json
+from datetime import datetime
+from itertools import islice
+
+import rethinkdb as r
+import tornado.web
+from more_itertools import chunked
+
+
+class RethinkDBHandler(tornado.web.RequestHandler):
+    def initialize(self):
+        self.conn = r.connect(self.application.settings['host'],
+                              self.application.settings['port'])
+
+
+class PointOfInterestHandler(RethinkDBHandler):
+    def get(self, *args):
+        self.set_header('Content-Type', 'application/json')
+        center = r.point(*map(float, args))
+        selection = r.table('osm_pois').get_nearest(
+            center, index='geometry', max_results=20, unit='mi')
+        start_t = datetime.now()
+        array = selection.map(lambda row: {
+            'type': 'Feature',
+            'geometry': row['doc']['geometry'].to_geojson(),
+            'properties': row['doc']['properties'].merge(
+                {'distance': row['dist']}),
+        }).run(self.conn)
+        total_t = (datetime.now() - start_t).total_seconds()
+        print 'POI query took', total_t, 's and provided', len(array), 'results'
+        self.write(json.dumps(array))
+
+
+class OSMHandler(RethinkDBHandler):
+
+    def write_event(self, data):
+        self.write('data: {}\n\n'.format(json.dumps(data)))
+        self.flush()
+
+    def get(self, *args):
+        self.set_header('Content-Type', 'text/event-stream')
+        north, south, east, west = map(float, args)
+        start_t = datetime.now()
+        query_range = r.polygon(
+            r.point(north, west),
+            r.point(south, west),
+            r.point(south, east),
+            r.point(north, east))
+        selection = (r.table('osm_nopoints')
+                     .get_intersecting(query_range, index='geometry'))
+        initial_t = (datetime.now() - start_t).total_seconds()
+        cursor = selection.map(r.row['geometry'].to_geojson()).run(self.conn)
+        size = 0
+        for chunk in chunked(cursor, 500):
+            size += len(chunk)
+            self.write_event(chunk)
+        self.write_event('done')
+        total_t = (datetime.now() - start_t).total_seconds()
+        print 'OSM query took', initial_t, 's for the first batch',
+        print '(', total_t, 's total) and provided', size, 'results.'
+
+
+def main(port, host, rport):
+    settings = {'host': host, 'port': rport}
+    routes = [
+        (r'/osm/(.*?)/(.*?)/(.*?)/(.*?)/?', OSMHandler),
+        (r'/poi/(.*?)/(.*?)/?', PointOfInterestHandler),
+        (r'/(.*)', tornado.web.StaticFileHandler, {'path': 'static'}),
+    ]
+    tornado.web.Application(routes, **settings).listen(port)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('port', type=int, default=8090,
+                        help='Port server should run on')
+    parser.add_argument('--rhost', default='localhost',
+                        help='RethinkDB hostname to connect to')
+    parser.add_argument('--rport', type=int, default=28015,
+                        help='RethinkDB port to connect to')
+    args = parser.parse_args()
+    main(args.port, args.rhost, args.rport)
+
+
---
+++ static/app.coffee
@@ -0,0 +1,91 @@
+L = require 'leaflet'
+$ = require 'jquery'
+
+# Leaflet needs to know where its icons are
+L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images/'
+
+# Create a map centered on RethinkDB offices
+map = L.map('map').setView [37.3873, -122.0663], 16
+
+# Tile-based map
+L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png',
+    maxZoom: 18
+    attribution: 'RethinkDB map demo 
+        | Map data &copy;
+        <a href="http://openstreetmap.org">OpenStreetMap</a>,
+        background imagery &copy;
+        <a href="http://mapbox.com">Mapbox</a>'
+    id: "danielmewes.j3ff1cj8"
+).addTo(map);
+
+#  Overlays
+myStyle = (g) ->
+    switch g.geometry.type
+        when 'Polygon' then color: "#ffa080", opacity: 0.01, weight: 4
+        when 'LineString' then color: "#0000ff", opacity: 0.2
+        
+streetLayer = L.geoJson([], style: myStyle).addTo map
+streetLayer.on "contextmenu", updatePOIs
+poisLayer = L.geoJson().addTo map
+
+# Refresh overlay whenever the map is redrawn
+updateStreetsId = 0
+updateStreets = (e) -> 
+    updateStreetsId++
+    streetLayer.clearLayers()
+    if map.getZoom() >= 15
+        ((_updateId) ->
+            # Load new data
+            bounds = map.getBounds()
+            [north, south] = [bounds.getNorth(), bounds.getSouth()]
+            [east, west] = [bounds.getEast(), bounds.getWest()]
+            jsonStream = new EventSource("/osm/#{north}/#{south}/#{east}/#{west}")
+            jsonStream.onmessage = (e) ->
+                if e.data == '"done"' or updateStreetsId != _updateId
+                    jsonStream.close()
+                else
+                    json = JSON.parse(e.data)
+                    streetLayer.addData(json)
+      )(updateStreetsId);
+
+map.on "load", updateStreets
+map.on "moveend", updateStreets
+map.on "zoomend", updateStreets
+
+# Gets POIs
+onEachFeature = (feature, layer) ->
+    if feature.properties
+        popupContent = "<p><b>#{feature.properties.name}</b><br>
+            Distance: #{Math.round(feature.properties.distance*10)/10} mi
+            mi</p>"
+        layer.bindPopup popupContent
+
+updatePOIsId = 0
+
+centerOverlay = null
+
+updatePOIs = (e) ->
+    if centerOverlay
+        map.removeLayer centerOverlay
+    centerOverlay = L.circleMarker [e.latlng.lat, e.latlng.lng],
+        fillColor: "#ff0000"
+        fillOpacity: 1
+        color: "#ff0000"
+        weight: 5
+    .addTo map
+
+    updatePOIsId++;
+    ((_updateId) ->
+        # Load new data
+        $.getJSON("/poi/#{e.latlng.lat}/#{e.latlng.lng}", (json) ->
+            if updatePOIsId == _updateId
+                myStyle = color: "#40f040", opacity: 0.5
+                newPoisLayer = L.geoJson json,
+                    style: myStyle
+                    onEachFeature: onEachFeature
+                # Swap the layers
+                map.removeLayer poisLayer
+                poisLayer = newPoisLayer
+                poisLayer.addTo map
+      ))(updatePOIsId);
+map.on "contextmenu", updatePOIs
